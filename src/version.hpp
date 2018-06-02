f091c7a3b5d52fca4d6b229465d652bd8767a752
mygirl8893 <minzai2012@hotmail.com>
2018-06-02 08:18:39 -0700

    Update version.hpp

--- src/version.hpp
+++ src/version.hpp
@@ -2,7 +2,7 @@
 
 // defines are for Windows resource compiler
 #define cryptonote_VERSION_WINDOWS_COMMA 2, 1, 4, 250
-#define cryptonote_VERSION_STRING "2.1.4-The Suniaster"
+#define cryptonote_VERSION_STRING "2.1.4-Tester"
 
 #ifndef RC_INVOKED  // Windows resource compiler
 
